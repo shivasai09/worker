c6599a0dcda29c247b916592e80d02ae0bcf5179
shivasai09 <33802398+shivasai09@users.noreply.github.com>
2022-03-28 10:39:08 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,25 @@
-console.log('hellow');
+self.onmessage = (event) => {
+
+    const num = event.data;
+
+    const result = fibonacci(num);
+
+ 
+
+    self.postMessage(result);
+
+    self.close();
+
+  };
+
+
+
+const fibonacci = (num) => {
+
+    if (num <= 1) return 1;
+
+ 
+
+    return fibonacci(num - 1) + fibonacci(num - 2);
+
+  };
